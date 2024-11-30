ddfa62c92ee1bff9c74ac97f7dbbc8b1b33cfcef
Screaper91 <155844020+Screaper91@users.noreply.github.com>
2024-11-30 17:14:05 -0500

    implement release schedule

--- common/config/server-config.js
+++ common/config/server-config.js
@@ -17,7 +17,34 @@ export default {
 		maxCards: 42,
 		maxDuplicates: 3,
 		maxDeckCost: 42,
-		bannedCards: ['evilxisuma_boss', 'feather'],
+		bannedCards: [
+			'evilxisuma_boss',
+			'feather',
+			'item_any_rare',
+			'full_bundle',
+			// Enable Dec 4th
+			'elder_guardian',
+			'allay',
+			// Enable Dec 7th
+			'tnt_minecart',
+			'arrow_of_poison',
+			// Enable Dec 11th
+			'redstone_torch',
+			'smithing_table',
+			// Enable Dec 14th
+			'sculk_catalyst',
+			'wind_burst',
+			'bundle',
+			// Enable Dec 18th
+			'zookeeperscar_rare',
+			'zookeeperscar_common',
+			// Enable Dec 21st
+			'cyberpunkimpulse_rare',
+			'cyberpunkimpulse_common',
+			// Enable Dec 25th
+			'postmasterpearl_rare',
+			'postmasterpearl_common',
+		],
 	},
 	logoSubText: 'Over 3 Games Played!',
 }
